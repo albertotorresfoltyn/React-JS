6a98fd0394f51f105a4953ba535206b1efe7db7c
Lorenzo Durante <lolodurante2003@gmail.com>
2021-03-09 12:41:29 -0300

    Proyecto Clases de manejo

---
+++ manejo/src/Navbar.js
@@ -0,0 +1,31 @@
+import './navbar.css'
+
+function Navegacion() {
+    return(
+        <nav>
+            
+            <div class="logo">
+                LOGO
+            </div>
+            <div class="items">
+                <div class="item">
+                    INICIO
+                </div>
+                <div class="item">
+                    NOSOTROS
+                </div>
+                <div class="item">
+                    CLASES
+                </div>
+                <div class="item">
+                    CONTACTANOS
+                </div>
+                <div class="item">
+                    CARRITO
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navegacion
---
+++ manejo/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Navegacion from './Navbar'
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Navegacion />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+reportWebVitals();
