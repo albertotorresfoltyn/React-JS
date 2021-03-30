18d7923a1cdeed6b719ed35789b8bf732f107a06
Lorenzo Durante <lolodurante2003@gmail.com>
2021-03-30 17:59:08 -0300

    Update carta.js

--- finalcoder/src/componentes/itemListContainer/carta.js
+++ finalcoder/src/componentes/itemListContainer/carta.js
@@ -1,24 +1,41 @@
-import React from 'react'
+import React,{useState}from 'react'
 import { Link } from 'react-router-dom'
 import './carta.css'
 import {InfoJson,InfoJson2} from'../info/info'
 
 
- function Item(props) {
-    return(
-        <div className="producto">
-            <img src={props.imagen} />
-            <div className="infoAbajo">
-                <div className="titulo">
-                    {props.titulo}
-                </div>
-                <div className="autor">
-                    {props.autor}
-                </div>
-                <Link to={`/producto/${props.id}`}><button>Ver mas</button></Link>
 
+ function Item(props) {
+    const[prod,setProd]=useState()
+    const [carga,setCarga]=useState(false)
+
+    useEffect(() => {
+        setIsLoading(true)
+        const promesa= new Promise((res, rej) => {      
+            setTimeout(() =>{
+                return(
+                <div className="producto">
+                <img src={props.imagen} />
+                <div className="infoAbajo">
+                    <div className="titulo">
+                        {props.titulo}
+                    </div>
+                    <div className="autor">
+                        {props.autor}
+                    </div>
+                    <Link to={`/producto/${props.id}`}><button>Ver mas</button></Link>
+    
+                </div>
             </div>
-        </div>
+            }
+            ),2000);    
+       });
+      promesa.then((result) => {      
+           setProd(result);     
+           setCarga(false);    
+      });
+},);
+
 
         
     )
